4914326858d28a58142cc00a35186ef062fa8ae4
ProgramPedro <113397385+ProgramPedro@users.noreply.github.com>
2025-03-16 10:16:41 -0400

    Fixed arithmetic for remove and add money. Changes now accurately reflects on the doughnut chart.

--- savings-goal.js
+++ savings-goal.js
@@ -1,4 +1,6 @@
 let totalSavings = document.getElementById("total-savings");
+let ogRemaining = 10000;
+
 let addMoney = document.getElementById("add-money");
 let removeMoney = document.getElementById("remove-money");
 
@@ -8,7 +10,6 @@ textEventListener(totalSavings);
 textEventListener(removeMoney);
 textEventListener(addMoney);
 
-
 let donutChart = new Chart( donutChartContext, {
     type: 'doughnut',
     data: {
@@ -17,7 +18,7 @@ let donutChart = new Chart( donutChartContext, {
             'Saved'
         ],
         datasets: [{
-            data: [10000, 500],
+            data: [10000, 0],
             backgroundColor: [
                 'rgb(144, 238, 144)',
                 'rgb(6, 64, 43)'
@@ -42,18 +43,40 @@ function textEventListener(actionName) {
                 actionName.value = "";
                 window.alert("ERROR: number typed is NOT a number! Please try again");
             } else {
+                
                 if (actionName == totalSavings) {
                     donutChart.data.datasets[0].data[0] = parseFloat(actionName.value);
+                    ogRemaining = parseFloat(actionName.value);
                     donutChart.update();
                     actionName.value = "";
+
                 } else if (actionName == removeMoney) {
-                    donutChart.data.datasets[0].data[1] = donutChart.data.datasets[0].data[1] - parseFloat(actionName.value);
-                    donutChart.update();
-                    actionName.value = "";
+                    if (donutChart.data.datasets[0].data[1] - parseFloat(actionName.value) < 0) {
+                        donutChart.data.datasets[0].data[1] = 0;
+                        /* Add a way for remove money to have an original total savings count!*/
+                        donutChart.data.datasets[0].data[0] = ogRemaining;
+                        donutChart.update();
+                        actionName.value = "";
+                    } else {
+                        donutChart.data.datasets[0].data[1] -= parseFloat(actionName.value);
+                        donutChart.data.datasets[0].data[0] += donutChart.data.datasets[0].data[1];
+                        donutChart.update();
+                        actionName.value = "";
+                    }
+
                 } else if (actionName == addMoney) {
-                    donutChart.data.datasets[0].data[1] = donutChart.data.datasets[0].data[1] + parseFloat(actionName.value);
-                    donutChart.update();
-                    actionName.value = "";
+                    if ((donutChart.data.datasets[0].data[1] + parseFloat(actionName.value)) > ogRemaining) {
+                        donutChart.data.datasets[0].data[1] = ogRemaining;
+                        donutChart.data.datasets[0].data[0] = 0;
+                        donutChart.update();
+                        actionName.value = "";
+                    } else {
+                        donutChart.data.datasets[0].data[1] += parseFloat(actionName.value);
+                        donutChart.data.datasets[0].data[0] = ogRemaining - donutChart.data.datasets[0].data[1];
+                        donutChart.update();
+                        actionName.value = "";
+                    }
+                    
                 }
             }
         }
