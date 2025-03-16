4522fc5cc4cdf9fd1a70b85ff5aad5d1fd3c2b78
ProgramPedro <113397385+ProgramPedro@users.noreply.github.com>
2025-03-16 00:43:44 -0400

    Added error handling for inputs; if not a number, an alert box will appear!

--- savings-goal.js
+++ savings-goal.js
@@ -37,20 +37,24 @@ function textEventListener(actionName) {
         if (event.key === "Enter") {
             //Prevent the form submission (if the input is inside a form)
             event.preventDefault();
-            //Clears the textbox when the user clicks the enter key
 
-            if (actionName == totalSavings) {
-                donutChart.data.datasets[0].data[0] = parseFloat(actionName.value);
-                donutChart.update();
+            if (isNaN(actionName.value)) {
                 actionName.value = "";
-            } else if (actionName == removeMoney) {
-                donutChart.data.datasets[0].data[1] = donutChart.data.datasets[0].data[1] - parseFloat(actionName.value);
-                donutChart.update();
-                actionName.value = "";
-            } else if (actionName == addMoney) {
-                donutChart.data.datasets[0].data[1] = donutChart.data.datasets[0].data[1] + parseFloat(actionName.value);
-                donutChart.update();
-                actionName.value = "";
+                window.alert("ERROR: number typed is NOT a number! Please try again");
+            } else {
+                if (actionName == totalSavings) {
+                    donutChart.data.datasets[0].data[0] = parseFloat(actionName.value);
+                    donutChart.update();
+                    actionName.value = "";
+                } else if (actionName == removeMoney) {
+                    donutChart.data.datasets[0].data[1] = donutChart.data.datasets[0].data[1] - parseFloat(actionName.value);
+                    donutChart.update();
+                    actionName.value = "";
+                } else if (actionName == addMoney) {
+                    donutChart.data.datasets[0].data[1] = donutChart.data.datasets[0].data[1] + parseFloat(actionName.value);
+                    donutChart.update();
+                    actionName.value = "";
+                }
             }
         }
     });
